48c7e37700cb370b2e7d859b6f5014021cfffbfc
OleksandrHridzhak <sgridzak@gmail.com>
2025-11-02 19:52:58 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LAB8_DODATKOVE
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button2
+      // 
+      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.button2.Image = global::LAB8_DODATKOVE.Properties.Resources.Screenshot_2025_11_02_192053;
+      this.button2.Location = new System.Drawing.Point(442, 13);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(426, 301);
+      this.button2.TabIndex = 1;
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.button3.Image = global::LAB8_DODATKOVE.Properties.Resources.Screenshot_2025_11_02_192115;
+      this.button3.Location = new System.Drawing.Point(874, 13);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(465, 302);
+      this.button3.TabIndex = 2;
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoEllipsis = true;
+      this.label1.Location = new System.Drawing.Point(12, 326);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(412, 246);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "label1";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoEllipsis = true;
+      this.label2.Location = new System.Drawing.Point(437, 336);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(426, 256);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "label2";
+      // 
+      // label3
+      // 
+      this.label3.AutoEllipsis = true;
+      this.label3.Location = new System.Drawing.Point(869, 336);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(470, 273);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "label3";
+      // 
+      // button1
+      // 
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.button1.Image = global::LAB8_DODATKOVE.Properties.Resources.Screenshot_2025_11_02_191823;
+      this.button1.Location = new System.Drawing.Point(12, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(412, 299);
+      this.button1.TabIndex = 0;
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1362, 641);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB8_DODATKOVE
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    public abstract class President
+    {
+      public string Name { get; set; }
+      public Label HisLabel { get; set; }
+      public abstract void Slogan();
+    }
+
+    public interface IVticach
+    {
+      string KrainaPerehovyvania { get; set; }
+      void Tikaty(string VtikatyV);
+
+    }
+
+    public class Zelensky: President
+    {
+      public Zelensky(Label label)
+      {
+        Name = "Володимир Зеленський";
+        HisLabel = label;
+      }
+      public override void Slogan()
+      {
+        HisLabel.Text = "Ща позвоню Баканову по поводу этого черта";
+      }
+    }
+    public class Poroshenko : President
+    {
+      public Poroshenko(Label label)
+      {
+        Name = "Петро Порошенко";
+        HisLabel = label;
+      }
+
+      public override void Slogan()
+      {
+        HisLabel.Text = "Прощай, немытая Россия";
+      }
+    }
+
+    public class Yanukovych : President
+    {
+      public Yanukovych(Label label)
+      {
+        Name = "Віктор Янукович";
+        HisLabel = label;
+      }
+
+      public override void Slogan()
+      {
+        HisLabel.Text = "ви забили? забули про це? я вам трошки напам'ятаю";
+      }
+    }
+
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
