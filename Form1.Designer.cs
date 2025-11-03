2cb75edfbe77401b59b5c09250290b5859da3081
OleksandrHridzhak <sgridzak@gmail.com>
2025-11-03 07:36:57 +0200

    Added 4 interfaces for our presidents

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -109,7 +109,7 @@
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
       this.Name = "Form1";
-      this.Text = "Form1";
+      this.Text = "Ukrainian presidents simulator";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
 
--- Form1.cs
+++ Form1.cs
@@ -29,9 +29,26 @@ namespace LAB8_DODATKOVE
       void Tikaty(string VtikatyV);
 
     }
-
-    public class Zelensky: President
+    public interface IRussianBusinessSeller 
+    { 
+    
+      void Prodavat(string shoProdavat);
+    }
+    public interface IVidzhumach
     {
+      string BussinesDlaVidzhumania { get; set; }
+      void Vidzhymaty(string shchoVidzhymaty);
+    }
+    public interface IDemocracyAtacker
+    {
+      string MethodOfAttack { get; set; }
+      void Atakuvaty(string shchoAtakuvaty);
+    }
+
+    public class Zelensky: President, IVidzhumach, IDemocracyAtacker
+    {
+      public string BussinesDlaVidzhumania { get; set; }
+      public string MethodOfAttack { get; set; }
       public Zelensky(Label label)
       {
         Name = "Володимир Зеленський";
@@ -39,25 +56,51 @@ namespace LAB8_DODATKOVE
       }
       public override void Slogan()
       {
-        HisLabel.Text = "Ща позвоню Баканову по поводу этого черта";
+        HisLabel.Text = Name + " каже: "  + "Щаc позвоню Баканову по поводу этого черта";
+      }
+      public void Vidzhymaty(string shchoVidzhymaty)
+      {
+        BussinesDlaVidzhumania = shchoVidzhymaty;
+        HisLabel.Text += "\n" + Name + " виджимає " + BussinesDlaVidzhumania;
+      }
+      public void Atakuvaty(string shchoAtakuvaty)
+      {
+        MethodOfAttack = shchoAtakuvaty;
+        HisLabel.Text += "\n" + Name + " атакує " + MethodOfAttack;
       }
     }
-    public class Poroshenko : President
+    public class Poroshenko : President, IRussianBusinessSeller
     {
       public Poroshenko(Label label)
       {
         Name = "Петро Порошенко";
         HisLabel = label;
       }
+      public void Prodavat(string shoProdavat)
+      {
+        HisLabel.Text += "\n" + Name + " продає " + shoProdavat;
+      }
 
       public override void Slogan()
       {
-        HisLabel.Text = "Прощай, немытая Россия";
+        HisLabel.Text = Name + " каже: " + "Прощай, немытая Россия";
       }
     }
 
-    public class Yanukovych : President
+    public class Yanukovych : President, IVticach, IDemocracyAtacker
     {
+      public string KrainaPerehovyvania { get; set; }
+      public string MethodOfAttack { get; set; }
+      public void Tikaty(string VtikatyV)
+      {
+        KrainaPerehovyvania = VtikatyV;
+        HisLabel.Text += "\n" + Name + " втікає в " + KrainaPerehovyvania;
+      }
+      public void Atakuvaty(string shchoAtakuvaty)
+      {
+        MethodOfAttack = shchoAtakuvaty;
+        HisLabel.Text += "\n" + Name + " атакує " + MethodOfAttack;
+      }
       public Yanukovych(Label label)
       {
         Name = "Віктор Янукович";
@@ -66,7 +109,7 @@ namespace LAB8_DODATKOVE
 
       public override void Slogan()
       {
-        HisLabel.Text = "ви забили? забули про це? я вам трошки напам'ятаю";
+        HisLabel.Text = Name + " каже: " + "ви забили? забули про це? я вам трошки напам'ятаю";
       }
     }
 
@@ -80,21 +123,31 @@ namespace LAB8_DODATKOVE
 
     private void button1_Click(object sender, EventArgs e)
     {
-      
+      Zelensky zelensky = new Zelensky(label1);
+      zelensky.Slogan();
+      zelensky.Vidzhymaty("PrivatBank");
+      zelensky.Atakuvaty("НАБУ");
+
     }
 
     private void label1_Click(object sender, EventArgs e)
     {
-
+      
     }
 
     private void button2_Click(object sender, EventArgs e)
     {
-
+      Poroshenko poroshenko = new Poroshenko(label2);
+      poroshenko.Slogan();
+      poroshenko.Prodavat("Roshen");
     }
 
     private void button3_Click(object sender, EventArgs e)
     {
+      Yanukovych yanukovych = new Yanukovych(label3);
+      yanukovych.Slogan();
+      yanukovych.Tikaty("Росію");
+      yanukovych.Atakuvaty("Євромайдан");
 
     }
   }
